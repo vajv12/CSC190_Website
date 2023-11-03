8de533474ff6f5aab981ab1e78a395bf0cc827a8
Dzmitry M <ma.dzmitry@gmail.com>
2023-11-03 08:35:29 -0700

    Update Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,12 @@
 // src/Menu.js
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from React Router
 
 function Menu() {
   return (
     <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
     </ul>
   );
 }
