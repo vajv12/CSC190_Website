3273a4500967c3c5327720ca71ae05b8996ecd2f
Christian561 <chrislong561@gmail.com>
2023-11-12 22:07:53 -0800

    Jira Task #71: Button that links to GEG's Point.com catalogue
    Basic button that can be used to link to the client's product catalogue that they are already using on Point.com

---
+++ src/components/BrowseProductsButton.js
@@ -0,0 +1,10 @@
+// src/BrowseProductsButton.js
+import React from 'react';
+
+function BrowseProductsButton(props) {
+  return (
+    <a href="https://www.pointy.com/shops/usa/california/sacramento/great-escape-games" target="_blank"><button>Browse Products</button></a>
+  );
+}
+
+export default BrowseProductsButton;
