7456513f107db96559bce0bbb8f36c0929b972d2
Ancxo <achouhan0114@gmail.com>
2023-11-12 22:22:29 -0800

    using routing for pages as well as added a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
 // src/App.js
-import React from 'react';
-import Button from './Button';
 import './App.css'; // CSS for styling
-
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from "./navbar/navbar";
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import Contact from './components/contact/contact'
+import Cart from './pages/Cart'
+import Event from './pages/Event'
 
 function App() {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
-
- 
-
-
-return (
+  return (
     <div className="App">
-      <div className="menu">
-        <ul>
-          <li><Button label="Button 1" onClick={handleClick} /></li>
-           <li><Button label="Button 2" onClick={handleClick} /></li>
-           <li><Button label="Button 3" onClick={handleClick} /></li>
-           <li><Button label="Button 4" onClick={handleClick} /></li>
-          <li><input type="text" placeholder="Search" /></li>
-        </ul>
-      </div>
-      <div className="content">
-<img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg" alt="Mountain" className="image" />
-      </div>
-
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/Shop" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path ="/event" element={<Event />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
-
-
-
-
 }
-
+  
 export default App;
---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="shop">
+      </div>
+      <div className="links">
+        <Link to="/Home"> Home Page </Link>
+        <Link to="/Shop"> Shop All </Link>
+        <Link to="/Events"> Events </Link>
+        <Link to="/About"> About Us </Link>
+        <Link to="/Contact"> Contact </Link>
+        <Link to="/Cart"> Cart </Link>
+      </div>
+    </div>
+  );
+};
