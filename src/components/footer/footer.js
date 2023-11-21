26e973c1a30b560eeff4f47cde810c10179301ae
Shari <96545440+sharinguyen@users.noreply.github.com>
2023-11-21 00:21:27 -0800

    contact page center/resized. Made a image folder

---
+++ src/components/footer/footer.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+import "./footer.css";
+
+ const footer = () => {
+  return (
+    <footer>
+       
+    <p>Contact: 
+    <br/>
+       <a>Sacramento Phone:916 927 0810 </a>
+        <br/>
+        <a>Rocklin Phone: 916 927 0810 </a>
+        
+    </p>
+    <p>
+        Locations:
+        <br/>
+        <a href="Sacramento">Sacramento </a>
+        <br/>
+        <a href="Rocklin "> Rocklin</a>
+    </p>
+
+    <p>
+        Follow us: 
+    
+       
+
+        <a href="https://www.facebook.com/GEGsacramento" 
+        target="_blank"/>
+            <img src="socialMediaLogo/Facebook_Logo_2023.png" alt="Facebook"/>
+
+        <a href="https://www.yelp.com/biz/great-escape-games-sacramento" 
+        target="_blank"/>
+            <img src="socialMediaLogo/yelp-logo.png" alt="yelp"/>
+       
+
+    <br />
+
+        <a href="https://discord.com/invite/QqpvvqK" 
+        target="_blank">
+            <img src="socialMediaLogo/discord-logo.png" alt="discord"/>
+        </a>
+
+        <a href="https://www.instagram.com/explore/locations/252481501/great-escape-games-and-comics/?hl=en" 
+        target="_blank">
+        <img src="socialMediaLogo/instagram-logo.png" alt="Instagram"/>
+        </a>
+       
+    </p>
+
+    <p>
+        <a href="#">Home</a>
+        <br />
+        <a href="#">About</a>
+        <br />
+        <a href="#">Services</a>
+        <br />
+        <a href="#">Contact</a>
+        <br />
+      
+        <a href="..\hours\hours.html">Hours</a>
+    </p>
+   
+  
+
+
+<p>&copy; 2023 Great Escape Games. All rights reserved.</p>
+</footer>
+  
+
+   
+  );
+};
+
+export default footer;
