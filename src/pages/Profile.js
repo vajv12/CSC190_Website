9eb31269c1562eda29f76443eec8f72e1f8b6a0b
adawatan <adawatan94@gmail.com>
2023-12-01 01:59:53 -0800

    #Jira task number: 54 #what this fix does Added the page for the profile

---
+++ src/pages/Profile.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import './Profile.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Profile() {
+    
+  return (
+    <div>
+			<div class="container">
+<div class="row justify-content-center">
+    <div class="col-12 col-lg-10 col-xl-8 mx-auto">
+        <h2 class="h3 mb-4 page-title">Profile settings</h2>
+        <div class="my-4">
+            <form>
+                <div class="row mt-5 align-items-center">
+                    <div class="col-md-3 text-center mb-5">
+                        <div class="avatar avatar-xl">
+                            <img src="/image/testprofile.jpg" alt="..." class="avatar-img rounded-circle" />
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="row align-items-center">
+                            <div class="col-md-">
+                                <h4 class="mb-1">John, doe</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-4" />
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="firstname">Firstname</label>
+                        <input type="text" id="firstname" class="form-control" placeholder="John" />
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="lastname">Lastname</label>
+                        <input type="text" id="lastname" class="form-control" placeholder="doe" />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="johndoe@gmail.com" />
+                </div>
+                <div class="form-group">
+                    <label for="inputAddress5">Address</label>
+                    <input type="text" class="form-control" id="inputAddress5" placeholder="Sac state" />
+                </div>
+                <div class="form-group">
+                    <label for="inputPhone">Phone Number</label>
+                    <input type="text" class="form-control" id="inputphonenum" placeholder="123456789" />
+                </div>
+                <hr class="my-4" />
+                <div class="row mb-4">
+                    <div class="col-md-6">
+                        <p>Change Password</p>
+                        <div class="form-group">
+                            <label for="inputPassword4">Old Password</label>
+                            <input type="password" class="form-control" id="inputPassword5" />
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPassword5">New Password</label>
+                            <input type="password" class="form-control" id="inputPassword5" />
+                        </div>
+                        <div class="form-group">
+                            <label for="inputPassword6">Confirm Password</label>
+                            <input type="password" class="form-control" id="inputPassword6" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="mb-2">Password requirements</p>
+                        <p class="small text-muted mb-2">To create a new password, you have to meet all of the following requirements:</p>
+                        <ul class="small text-muted pl-4 mb-0">
+                            <li>Minimum 8 character</li>
+                            <li>At least one special character</li>
+                            <li>At least one number</li>
+                            <li>Can’t be the same as a previous password</li>
+                        </ul>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Save Change</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+</div>
+	</div>
+)
+}
+
+export default Profile
