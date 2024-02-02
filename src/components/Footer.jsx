d8a3c8cab4062cc514b74d279cf166688c010862
Shari <96545440+sharinguyen@users.noreply.github.com>
2024-02-02 00:54:30 -0800

    Update Footer.jsx
    deleted useState in import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 //import { Link } from 'react-router-dom';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FaceBookIcon from '@mui/icons-material/Facebook';
