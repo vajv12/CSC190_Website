0d752812fb80eef4ed267eee09860633635ce50b
Shari <96545440+sharinguyen@users.noreply.github.com>
2024-02-01 22:41:42 -0800

    deleting files, some will be reorganize in another spot

--- src/pages/Cart.js
+++
@@ -1,5 +0,0 @@
-export default function Cart(){
-    return(
-        <h1> cart </h1>
-    )
-}
--- src/pages/Create.js
+++
@@ -1,7 +0,0 @@
-export default function Create(){
-    // A drop down menu to see what you want to create on the website. 
-    //Product, News, event, etc
-    return(
-        <h1> Create </h1>
-    )
-}
