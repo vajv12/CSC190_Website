e5697fdece1cf6895250c422d40d63e9f3c2c3c8
Shari <96545440+sharinguyen@users.noreply.github.com>
2024-02-19 22:15:52 -0800

    GEG-105
    edited about us page and error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from './layouts/MainLayout.jsx';
 import AdminLayout from './layouts/AdminLayout.jsx';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from './components/Navbar.jsx';
 
 import Home from './pages/Home.jsx';
@@ -15,28 +14,126 @@ import Admin from './admin/Admin.jsx';
 import Login from './pages/LoginSignUp.jsx';
 import Cart from './pages/Cart.jsx';
 import Locations from './pages/Locations.jsx';
-import RocklinHours from './pages/RocklinHours.jsx';
+import Rocklin from './pages/Rocklin.jsx';
 import PrivateRooms from './pages/PrivateRooms.jsx';
 import Product from './pages/Product.jsx';
+import Payment from './pages/Payment.js';
+import { FirebaseProvider } from './FirebaseContext.js'; 
+
+//**************************** Start of Firebase Initialization************************************* */
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getAuth} from "firebase/auth";
+// Import the functions you need from the SDKs you nee
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTmAvGOetZpKVXBDQyOG18Y8YfVOVPV3M",
+  authDomain: "csc190-w.firebaseapp.com",
+  databaseURL: "https://csc190-w-default-rtdb.firebaseio.com",
+  projectId: "csc190-w",
+  storageBucket: "csc190-w.appspot.com",
+  messagingSenderId: "59153071763",
+  appId: "1:59153071763:web:dbb5fd713ef03b6353d766",
+  measurementId: "G-KYZHBDYFBR"
+};
+
+// Initialize Firebase
+let isAuthenticated = false;
+let loggedInUser;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+
+//observer that gets called when user logs in or logs out
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in.
+    isAuthenticated = true;
+    loggedInUser = user;
+  } else {
+    // No user is signed in.
+    isAuthenticated = false;
+    loggedInUser = null;
+  }
+});
+
+//**************************** End of Firebase Initialization************************************* */
+const analytics = getAnalytics(app);
+
+
 function App() {
   return (
     <div className="App">
 
         <BrowserRouter>
-          <Navbar />
           <Routes>
-            <Route path="/pages/Home" element={<Home />} />
-            <Route path="/pages/Shop" element={<Shop />} />
-            <Route path="/pages/Contact" element={<Contact />} />
-            <Route path="/pages/About" element={<About />} />
-            <Route path="/pages/Admin" element={<Admin />} />
-            <Route path="/pages/Login" element={<Login />} />
-            <Route path="/pages/Locations" element={<Locations />}/>
-            <Route path="/pages/Rocklin" element ={<Rocklin />}/>
-            <Route path="/pages/PrivateRooms" element ={<PrivateRooms />}/>
-            <Route path="/pages/Product" element ={<Product/>}/>
+                  {/*uses the teal color for header for all pages */}
+              
+            <Route
+              path="/pages/*"
+              element={
+                <MainLayout>
+                  {/* routes to pages in main */}
+                  <Routes>
+                    <Route path="/pages/Home" element={<Home />} />
+                    <Route path="/pages/Shop" element={<Shop />} />
+                    <Route path="/pages/Contact" element={<Contact />} />
+                    <Route path="/pages/About" element={<About />} />
+                    <Route path="/pages/Login" element={<Login />} />
+                    <Route path="/pages/Cart" element={<Cart />} />
+                    <Route path="/pages/Locations" element={<Locations />} />
+                    <Route path="/pages/Rocklin" element={<Rocklin />} />
+                    <Route path="/pages/PrivateRooms" element={<PrivateRooms />} />
+                    <Route path="/pages/Product" element={<Product />} />
+                  </Routes>
+                </MainLayout>
+              }
+            />
+            {/* changes header to grey and takes nav links  to distinguish change */}
+            <Route
+              path="/admin/Admin"
+              element={
+                <AdminLayout>
+                  <Routes>
+                    <Route path="/" element={<Admin />} />
+                 
+                  </Routes>
+                </AdminLayout>
+              }
+            />
           </Routes>
         </BrowserRouter>
+
+      <div className="content">
+      </div>
+        <FirebaseProvider 
+          auth={auth} 
+          db={db} 
+          isAuthenticated={isAuthenticated} 
+          username={loggedInUser ? loggedInUser.displayName : ""}
+        >
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/pages/Home" element={<Home />} />
+              <Route path="/pages/Shop" element={<Shop />} />
+              <Route path="/pages/Contact" element={<Contact />} />
+              <Route path="/pages/About" element={<About />} />
+              <Route path="/pages/Admin" element={<Admin />} />
+              <Route path="/pages/Login" element={<Login />} />
+              <Route path="/pages/Locations" element={<Locations />}/>
+              <Route path="/pages/Rocklin" element ={<Rocklin />}/>
+              <Route path="/pages/PrivateRooms" element ={<PrivateRooms />}/>
+              <Route path="/pages/Product" element ={<Product/>}/>
+              <Route path="/pages/Payment" element ={<Payment/>}/>
+            </Routes>
+          </BrowserRouter>
+        </FirebaseProvider>
         <Footer />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import Dropdownlocations from "../helpers/DropdownLocations.jsx";
 //Icons install with npm install @mui/material @emotion/react @emotion/styled
 //and npm install @mui/icons-material
 import AccountBox from '@mui/icons-material/AccountBox';
-import ShoppingCart from '@mui/icons-material/ShoppingCart';
+import PaymentIcon from '@mui/icons-material/Payment';
 import ReorderIcon from'@mui/icons-material/Reorder';
 
 function Navbar() {
@@ -30,6 +30,7 @@ return (
         <Link to ='./pages/Home'>
         <img src={Logo} alt="Great Escape Games Logo"/>
         </Link>
+
         <div className="hiddenLinks">
             <Link to ='./pages/Home'>Home</Link>
             <Link to ='./pages/Product'>Product</Link>
@@ -40,7 +41,6 @@ return (
             <Link to ='./pages/PrivateRooms'>Private Rooms</Link>
             <Link to ='./pages/About'>About</Link>
             <Link to ='./pages/Contact'>Contact</Link>
-            <Link to ='./pages/Payment'>Payment</Link>
         </div>
 
         </div>
@@ -55,12 +55,15 @@ return (
             <Link to ='./pages/PrivateRooms'>Private Rooms</Link>
             <Link to ='./pages/About'>About</Link>
             <Link to ='./pages/Contact'>Contact</Link>
+
+            <Link to='./pages/Payment'>
+           <PaymentIcon  alt="payment card"/>
+           </Link>
+
             <Link to='./pages/Login'>
           <AccountBox alt="account icon" />
         </Link>
-        <Link to='./pages/About'>
-           <ShoppingCart  alt="shopping cart"/>
-           </Link>
+   
            
 {/*When screen is small itll show the reorder button that is toggle on and off, uses hiddenlink className */}
            <button onClick={toggleNavbar}>
