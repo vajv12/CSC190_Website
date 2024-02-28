f581ca3d1ca3d1cf41f029c886a6a0283f0c159a
Dzmitry M <ma.dzmitry@gmail.com>
2024-02-27 16:08:42 -0800

    updated footer
    based on the final design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FaceBookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+
 
 import '../styles/Footer.css';
+import DiscordIcon from '../assets/discord.png';
 
 function Footer() {
 
@@ -25,38 +28,85 @@ function Footer() {
   //   // Reset the email input
   //   setEmail('');
   // };
+
   return (
-    <div className="footer">
-      <div className="socialMedia">
-        {/* Instagram */}
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank" rel="noopener noreferrer">
-          <InstagramIcon />
-        </a>
+    <footer className="footer">
+      <div className="footer-column">
+        <nav className="footer-nav">
+          <a href="/products">Products</a>
+          <a href="/about">About</a>
+          <a href="https://greatescapegamesllc.tcgplayerpro.com/">Card Finder</a>
+          <a href="/support">Support</a>
+          <a href="/feedback">Feedback</a>
+        </nav>
+        <div className="social">
 
-        {/* Facebook */}
-        <a href="https://www.facebook.com/GEGsacramento" target="_blank" rel="noopener noreferrer">
-          <FaceBookIcon />
-        </a>
+          <div className="social-icons">
 
-        {/* Twitter */}
-        <a href="https://twitter.com/?lang=en" target="_blank" rel="noopener noreferrer">
-          <TwitterIcon />
-        </a>
+            {/* Icons */}
+            <a href="https://www.instagram.com/explore/locations/252481501/great-escape-games-and-comics/?hl=en" target="_blank" rel="noopener noreferrer">
+              <InstagramIcon />
+            </a>
+            <a href="https://www.facebook.com/GEGsacramento" target="_blank" rel="noopener noreferrer"><FaceBookIcon />
+            </a>
+
+            <a href="https://discord.com/invite/QqpvvqK">
+              <img src={DiscordIcon} alt="Description" style={{ width: 24, filter: "invert(100%)" }} />
+            </a>
+
+
+
+
+            <a href="https://www.youtube.com/channel/UCLXZYz1W28wHGWhjYSmiPtQ/videos" target="_blank" rel="noopener noreferrer"><YouTubeIcon /> </a>
+          </div>
+        </div>
       </div>
-      <p>&copy; 2024 Great Escape Games</p>
 
-{/* work on later to create a subscribe email option also commented out in css */}
-        {/* Email subscription form 
-      <form onSubmit={handleSubscribe}>
-        <label>
-          Subscribe to Updates:
-          <input type="email" value={email} onChange={handleEmailChange} required />
-        </label>
-        <button type="submit">Subscribe</button>
-      </form>
-       */}
-    </div>
+      <div className="footer-column">
+        
+        <h2>Sacramento Location:</h2>
+        <p>Phone: 916 927 0810</p>
+        <p>1250 Howe Ave #3a, Sacramento, CA 95825</p>
+
+        <br />
+
+        <h2>Rocklin Location:</h2>
+        <p>Phone: 916 259 1797</p>
+        <p>5050 Rocklin Road, Suite A22, Rocklin, CA 95677</p>
+      </div>
+
+      <div className="footer-column">
+
+
+        <h2>Sacramento</h2>
+        <p>Mon 12pm - 8pm</p>
+        <p>Tue 12pm - 10pm</p>
+        <p>Wed 12pm - 8pm</p>
+        <p>Thu 12pm - 10pm</p>
+        <p>Fri 12pm - 11pm</p>
+        <p>Sat 12pm - 10pm</p>
+        <p>Sun 12pm - 6pm</p>
+
+
+      </div>
+
+      <div className="footer-column">
+
+
+        <h2>Rocklin</h2>
+        <p>Mon 12pm - 7pm</p>
+        <p>Tue 12pm - 7pm</p>
+        <p>Wed 12pm - 9pm</p>
+        <p>Thu 12pm - 7pm</p>
+        <p>Fri 12pm - 11pm</p>
+        <p>Sat 12pm - 10pm</p>
+        <p>Sun 12pm - 6pm</p>
+
+      </div>
+    </footer>
   );
+
+
 }
 
 export default Footer;
