e9f004c11e73c0477c602b827fd9006679e70a2f
Dzmitry M <ma.dzmitry@gmail.com>
2024-02-29 16:00:48 -0800

    updating slideshow

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,37 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Slideshow.css';
 
-
 const Slideshow = ({ images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex((currentImageIndex) => (currentImageIndex + 1) % images.length);
+      setCurrentImageIndex(currentImageIndex => (currentImageIndex + 1) % images.length);
     }, 4000); // Change image every 4 seconds
 
     return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, [currentImageIndex, images.length]); // Update to include dependencies
+  }, [currentImageIndex, images.length]);
 
-  const nextImage = () => {
-    setCurrentImageIndex((currentImageIndex + 1) % images.length);
-  };
-
-  const prevImage = () => {
-    setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
+  const selectSlide = (index) => {
+    setCurrentImageIndex(index);
   };
 
   return (
     <div className="slideshow">
       {images.map((image, index) => (
-        <div
-          key={image}
-          className={`slide ${index === currentImageIndex ? 'active' : ''}`}
-          style={{ backgroundImage: `url(${image})` }}
-        ></div>
+        <div key={image} className={`slide ${index === currentImageIndex ? 'active' : ''}`} style={{ backgroundImage: `url(${image})` }}>
+          <div className="slide-label">Slide {index + 1}</div> {/* Label added here */}
+        </div>
       ))}
-      <button onClick={prevImage} className="slide-btn prev">&#10094;</button>
-      <button onClick={nextImage} className="slide-btn next">&#10095;</button>
+      
+      <div className="slide-navigation">
+        {images.map((_, index) => (
+          <button key={index} onClick={() => selectSlide(index)} className={`nav-button ${index === currentImageIndex ? 'active' : ''}`}></button>
+        ))}
+      </div>
     </div>
   );
 };
