89184ac7cce0e4b5e6e5ec0d4df0b44cdc2d8499
Dzmitry M <ma.dzmitry@gmail.com>
2024-03-17 20:25:41 -0700

    Visually changed NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,117 +1,49 @@
 import React, { useState } from "react";
-import Logo from "../assets/GEG-white.png";
+import Logo from "../assets/GEG-white-logo.png";
+
+
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import Dropdownlocations from "../helpers/DropdownLocations.jsx";
-
-
-//Icons install with npm install @mui/material @emotion/react @emotion/styled
-//and npm install @mui/icons-material
 import AccountBox from '@mui/icons-material/AccountBox';
-//import PaymentIcon from '@mui/icons-material/Payment';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
-
-    //set so that button can function
-    const [openLinks, setOpenLinks] = useState(false);
-
     const [searchTerm, setSearchTerm] = useState('');
 
-    const toggleNavbar = () => {
-        setOpenLinks(!openLinks);
-    };
-
-
-
     const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
+        const newSearchTerm = event.target.value;
+        setSearchTerm(newSearchTerm);
+        // Trigger your search logic here
+        console.log('Search Term:', newSearchTerm);
+        // You might want to debounce this operation if it's expensive or makes API calls
     };
-
-    const handleSearchSubmit = (event) => {
-        event.preventDefault(); // Prevent the form from causing a page reload
-        console.log('Search Term:', searchTerm);
-        // Here you can redirect or fetch search results based on searchTerm
-    };
-
-
 
     return (
-        /*navbar and link */
-        <div className="navbar" >
-            <div className="leftSide" id={openLinks ? "open" : "close"}>
-                {/*logo icon can traceback to home page */}
-                <Link to='/pages/Home'>
-                    <img src={Logo} alt="Great Escape Games Logo" />
-                </Link>
-
-                <div className="hiddenLinks">
-                    <Link to='/pages/Home'>Home</Link>
-                    <Link to='/pages/Product'>Products</Link>
-
-                    {/* Use the Dropdown locations */}
-                    < Dropdownlocations />
-
-                    <Link to='/pages/PrivateRooms'>Private Rooms</Link>
-                    <Link to='/pages/About'>About</Link>
-                    <Link to='/pages/Contact'>Contact</Link>
-                </div>
-
-            </div>
-            {/*pages link to respective spots. uses import page on top and link pages */}
-            <div className="rightSide">
+        <div className="navbar">
+            <Link to='/pages/Home'>
+                <img src={Logo} alt="Great Escape Games Logo" />
+            </Link>
+            <div className="links">
                 <Link to='/pages/Home'>Home</Link>
-
-
-                {/* The search bar */}
-                <form onSubmit={handleSearchSubmit} style={{ display: 'inline-block' }}>
-                    <input
-                        type="search"
-                        placeholder="Search..."
-                        value={searchTerm}
-                        onChange={handleSearchChange}
-                        style={{ 
-                            fontSize: 14,
-                            marginRight: '10px',
-                            backgroundColor: 'black',
-                            opacity: 0.4,
-                            border: 'none',
-                            outline: 'none',
-                            borderRadius: 15,
-                            padding: 10,
-                            color: 'white'
-                        }} // Styling is to moved to CSS file
-                    />
-                    <button type="submit">Search</button>
-                </form>
-                
                 <Link to='/pages/Product'>Products</Link>
-
-                {/* Use the Dropdown locations*/}
-                < Dropdownlocations />
-
+                <Dropdownlocations />
                 <Link to='/pages/PrivateRooms'>Private Rooms</Link>
                 <Link to='/pages/About'>About</Link>
                 <Link to='/pages/Contact'>Contact</Link>
-                {/* no payment needed done on the page 
-                <Link to='./pages/Payment'>
-                    <PaymentIcon alt="payment card" />
-                </Link>
-                    */}
+                <input
+                    type="search"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
                 <Link to='/pages/Login'>
-                    <AccountBox alt="account icon" />
-                </Link>
-
-                {/*When screen is small itll show the reorder button that is toggle on and off, uses hiddenlink className */}
-                <button onClick={toggleNavbar}>
-                    <ReorderIcon />
-                </button>
-
+                <AccountBox style={{ fontSize: '40px' }} alt="account icon" />
+                </Link> 
+                
             </div>
-
         </div>
     );
-
 }
 
 export default Navbar;
