f352f966c500c28361b8c37fdf1bea831b8f458c
Dzmitry M <ma.dzmitry@gmail.com>
2024-03-18 17:23:16 -0700

    added sign out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import '../styles/Navbar.css';
 import Dropdownlocations from "../helpers/DropdownLocations.jsx";
 import AccountBox from '@mui/icons-material/AccountBox';
 
-
 import { useFirebase } from '../FirebaseContext';
 
 function Navbar() {
@@ -34,6 +33,15 @@ function Navbar() {
         console.log('Search Term:', newSearchTerm);
     };
 
+    const handleSignOut = () => {
+        auth.signOut().then(() => {
+            console.log("Signed out successfully!");
+            window.location.reload();
+        }).catch((error) => {
+            console.error("Sign out error", error);
+        });
+    };
+
     return (
         <div className="navbar">
             <Link to='/pages/Home'>
@@ -53,7 +61,20 @@ function Navbar() {
                     onChange={handleSearchChange}
                 />
                 {user ? (
-                    <div>Hi, {user.displayName || "User"}</div> // Display "Hi, [User Name]" if signed in
+                    <div className="user-dropdown">
+                        Hi, {user.displayName || "User"}
+                        <div className="dropdown-content">
+                            <Link to="/pages/Profile" className="dropdown-item">Profile</Link>
+                            <Link to="/pages/MyOrders" className="dropdown-item">My Orders</Link>
+                            <button onClick={handleSignOut} className="dropdown-item" style={{
+                                border: 'none',
+                                background: 'transparent', margin: '8px', boxShadow: 'none',
+                                fontWeight: 'normal', fontSize: '21px'
+                            }}>Sign Out</button>
+
+
+                        </div>
+                    </div>
                 ) : (
                     <Link to='/pages/Login'>
                         <AccountBox style={{ fontSize: '40px' }} alt="account icon" />
